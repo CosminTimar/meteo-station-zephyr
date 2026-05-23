16f84546f9779317e474e47fdda0571039f8946e
CosminTimar <cosmintimar2@gmail.com>
2026-05-23 14:44:49 +0300

    add rain reading logic

--- src/mh_rd_sensor/mh_rd.h
+++ src/mh_rd_sensor/mh_rd.h
@@ -4,12 +4,26 @@
 
 #include "util.h"
 
+#define MH_RD_DATA_LENGHT  (1U)
+
+typedef enum{
+    DRY = 0,
+    LOW,
+    MEDIUM,
+    HEAVY,
+    ILLIGAL_ZONE
+}rain_intensity_e;
 
 static struct rain_internal_struct
 {
     bool convertion_done;
-    uint8_t rain_adc_conv;
+    rain_intensity_e rain_adc_conv;
 };
+
+
+void rain_sensor_init(void);
+
+void rain_worker(void);
 
 
 #endif
